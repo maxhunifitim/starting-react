8e5612d5b041f687c6162b9d895350872578e75b
Fitim Maxhuni <fmaxhuni57@gmail.com>
2023-01-07 22:55:47 +0100

    Introduction to React #7 | Css in JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
 
 const PokemonRow = ({ pokemon, onSelect }) => {
   console.log("OnSelect: ", onSelect);
@@ -53,6 +54,28 @@ PokemonInfo.propTypes = {
   }),
 };
 
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 2rem;
+`;
+
 function App() {
   const [filter, setFilter] = React.useState("");
   const [pokemon, setPokemon] = React.useState([]);
@@ -65,18 +88,12 @@ function App() {
   }, []);
 
   return (
-    <div style={{ margin: "auto", width: 800, paddingTop: "1rem" }}>
-      <h1 className="title">Pokemon Search</h1>
+    <Container>
+      <Title>Pokemon Search</Title>
 
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem",
-        }}
-      >
+      <TwoColumnLayout>
         <div>
-          <input
+          <Input
             type="text"
             value={filter}
             onChange={(evt) => setFilter(evt.target.value)}
@@ -107,8 +124,8 @@ function App() {
           </table>
         </div>
         {selectedItem && <PokemonInfo {...selectedItem} />}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
