af5cbb59140a43d04d1e1d67af7d9fbd48d9b18e
Fitim Maxhuni <fmaxhuni57@gmail.com>
2023-01-07 23:10:48 +0100

    Introduction to React #8 | Component Libraries

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 
+import { Button } from "@mui/material";
+
 const PokemonRow = ({ pokemon, onSelect }) => {
   console.log("OnSelect: ", onSelect);
   return (
@@ -10,7 +12,9 @@ const PokemonRow = ({ pokemon, onSelect }) => {
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
       <td>
-        <button onClick={() => onSelect(pokemon)}>Select!</button>
+        <Button variant="contained" onClick={() => onSelect(pokemon)}>
+          Select!
+        </Button>
       </td>
     </tr>
   );
